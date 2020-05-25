46855e1827faf669d6fb9345891c1361f06efb05
Chris <390074138@qq.com>
2020-05-25 15:41:23 -0700

    add email config

--- config/environment.rb
+++ config/environment.rb
@@ -6,3 +6,13 @@ Rails.application.initialize!
 ActionView::Base.field_error_proc = Proc.new do |html_tag, instance|
   html_tag.html_safe
 end
+
+ActionMailer::Base.smtp_settings = {
+  :user_name => ENV["SENDGRID_USERNAME"],
+  :password => ["SENDGRID_PASSWORD"],
+  :domain => "heroku.com",
+  :address => "smtp.sendgrid.net",
+  :port => 465,
+  :authentication => :plain,
+  :enable_starttls_auto => true,
+}
