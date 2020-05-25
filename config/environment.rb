af1b37d0a859ef59d4b755366a1e80d435d0d65f
Chris <390074138@qq.com>
2020-05-25 16:32:17 -0700

    email config

--- config/environment.rb
+++ config/environment.rb
@@ -9,7 +9,7 @@ end
 
 ActionMailer::Base.smtp_settings = {
   :user_name => ENV["SENDGRID_USERNAME"],
-  :password => ["SENDGRID_PASSWORD"],
+  :password => ENV["SENDGRID_PASSWORD"],
   :domain => "heroku.com",
   :address => "smtp.sendgrid.net",
   :port => 465,
