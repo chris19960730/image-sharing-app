7d57d6dd291f1ebd5b429cfb2deb1888b06a5406
Chris <390074138@qq.com>
2020-05-25 21:21:34 -0700

    email again

--- app/mailers/image_share_mailer.rb
+++ app/mailers/image_share_mailer.rb
@@ -1,9 +1,9 @@
 class ImageShareMailer < ApplicationMailer
   default from: "390074138chris@gmail.com"
 
-  def welcome_email
-    @image = params[:image]
-    @email = params[:email]
+  def welcome_email(image, email)
+    @image = image
+    @email = email
     @url = "https://getbootstrap.com/docs/4.4/content/images/"
     mail(to: @email, subject: "welcome to My awesome site!!")
   end
