a36a1720c7d18ed9122862a335668b20ea0e81e2
Chris <390074138@qq.com>
2020-05-25 17:19:54 -0700

    add different email config

--- config/environment.rb
+++ config/environment.rb
@@ -7,12 +7,12 @@ ActionView::Base.field_error_proc = Proc.new do |html_tag, instance|
   html_tag.html_safe
 end
 
-ActionMailer::Base.smtp_settings = {
-  :user_name => "api_key",
-  :password => ENV["SENDGRID_API_KEY"],
-  :domain => "heroku.com",
-  :address => "smtp.sendgrid.net",
-  :port => 465,
-  :authentication => :plain,
-  :enable_starttls_auto => true,
-}
+# ActionMailer::Base.smtp_settings = {
+#   :user_name => "api_key",
+#   :password => ENV["SENDGRID_API_KEY"],
+#   :domain => "heroku.com",
+#   :address => "smtp.sendgrid.net",
+#   :port => "465",
+#   :authentication => :plain,
+#   :enable_starttls_auto => true,
+# }
